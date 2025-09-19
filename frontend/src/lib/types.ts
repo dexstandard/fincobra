343e1306ea69050206f56af155a12cc51e7700b2
dexstandard <alxndrmann@gmail.com>
2025-09-19 12:07:14 +0700

    Fix frontend limit order status enum for TS

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -1,8 +1,11 @@
-export enum LimitOrderStatus {
-  Open = 'open',
-  Filled = 'filled',
-  Canceled = 'canceled',
-}
+export const LIMIT_ORDER_STATUS = {
+  Open: 'open',
+  Filled: 'filled',
+  Canceled: 'canceled',
+} as const;
+
+export type LimitOrderStatus =
+  (typeof LIMIT_ORDER_STATUS)[keyof typeof LIMIT_ORDER_STATUS];
 
 export interface LimitOrder {
   id: string;
